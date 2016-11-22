0522f0c04eaf34c86b4efb1850e6659a4f894ad5
Ragnyll <justanotherjakeyg@gmail.com>
2016-11-22 10:32:39 -0600

    updated startup scripts so its easier to see what its trying to do"

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+printf "============================== Checking all node packages are up to date =============================="
 npm install # makes sure all node packages are up to date
+printf "============================== Starting nginx =============================="
 sudo service start nginx # starts nginx
+printf "============================== Starting node =============================="
 node GServ/app.js # runs the node application
