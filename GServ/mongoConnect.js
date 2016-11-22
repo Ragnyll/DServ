de0cd26d0e42db56d498d873b4edba1fe8df9774
Ragnyll <justanotherjakeyg@gmail.com>
2016-11-22 14:08:50 -0600

    added a mongoConnect test

---
+++ GServ/mongoConnect.js
@@ -0,0 +1,13 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/GServ';
+
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
+});
