633762614779e1bb049eb1fbc6f285373499f487
Ragnyll <justanotherjakeyg@gmail.com>
2016-11-22 23:43:06 -0600

    moved beers to its own controller

--- GServ/app.js
+++ GServ/app.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 // deal with mongoose configurations for promises real quick
 mongoose.Promise = require('bluebird');
 mongoose.Promise = require('q').Promise;
-var Beer = require('../models/beer');
+var beerController = require('../controllers/beer');
 var port = process.env.PORT || 3000; // use environment port or just default to 3000
 
 var bodyParser = require('body-parser');
@@ -43,31 +43,25 @@ db.once('open', function() {
 var router = express.Router(); // route everyting through router
 app.use('/api', router); // router sits at the /api extension
 // middleware for all requests
-router.use(function(req, res, next){
+router.use(function(req, res, next) {
   console.log('Received a request');
   next();
 });
 
 router.get('/', function(req, res) {
-  res.json({message: 'Connected to GServ API'});
-});
-
-var beersRoute = router.route('/beers');
-beersRoute.post(function(req, res) {
-  console.log('in the beers route. trying to get all the users');
-
-  var beer = new Beer();
-  beer.name = req.body.name;
-  beer.type = req.body.type;
-  beer.quantity = req.body.quantity;
-  console.log('saved beer variables');
-  beer.save(function(err) {
-    if (err) {
-      res.send(err);
-    }
-    res.json({message: 'success!', data: beer});
+  res.json({
+    message: 'Connected to GServ API'
   });
 });
+
+router.route('/beers')
+  .post(beerController.postBeers)
+  .get(beerController.getBeers);
+
+router.route('/beers/:beer_id')
+  .get(beerController.getBeer)
+  .put(beerController.putBeer)
+  .delete(beerController.deleteBeer);
 
 // Get that server kickin
 app.listen(port, function() {
